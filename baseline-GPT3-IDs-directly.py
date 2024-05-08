08312cf388caf77fe2d6cab01e4a8f99ec7fc7f8
Marcelo Machado <marcelo.oc.machado@gmail.com>
2024-05-08 15:19:25 -0300

    Update to new version of openai api

--- baseline-GPT3-IDs-directly.py
+++ baseline-GPT3-IDs-directly.py
@@ -1,47 +1,53 @@
-import json
-import openai
-import ast
-from file_io import *
-from evaluate import *
-import time
 import argparse
+import ast
+import json
+import time
+
+from openai import OpenAI
+from openai.types.chat import ChatCompletion
+
+from evaluate import *
+from file_io import *
 
 
-def GPT3response(q):
-    response = openai.Completion.create(
+def GPT3response(q, client: OpenAI):
+    response: ChatCompletion
+    messages = [{'role': 'user', 'content': q}]
+    response = client.chat.completions.create(
         # curie is factor of 10 cheaper than davinci, but correspondingly less performant
-        model="text-davinci-003",
-        #model = "text-curie-001",
-        prompt=q,
+        model="gpt-4",
+        # model = "text-curie-001",
+        messages=messages,
         temperature=0,
         max_tokens=50,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
-    )    
-    response = response.choices[0].text
+    )
+    response = response.choices[0].message.content
     if response[0] == " ":
         response = response[1:]
-    try:    
+    try:
         response = ast.literal_eval(response)
     except:
         response = []
     return response
-        
+
 
 def run(args):
-    openai.api_key = args.oaikey
-    
+
+    client = OpenAI(api_key=args.oaikey, base_url='https://api.openai.com/v1/')
+
     prefix = '''State of Palestine, country-borders-country, ["Q801"]
     Paraguay, country-borders-country, ["Q155", "Q414", "Q750"]
     Lithuania, country-borders-country, ["Q34", "Q36", "Q159", "Q184", "Q211"]
-    ''' 
+    '''
 
     print('Starting probing GPT-3 ................')
 
     train_df = read_lm_kbc_jsonl_to_df(Path(args.input))
-    
-    print (train_df)
+
+    print(train_df)
 
     results = []
     for idx, row in train_df.iterrows():
@@ -51,7 +57,9 @@ def run(args):
             "SubjectEntityID": row["SubjectEntityID"],
             "SubjectEntity": row["SubjectEntity"],
             "Relation": row["Relation"],
-            "ObjectEntitiesID": GPT3response(prompt),  ## naming with IDs required for current evaluation script 
+            "ObjectEntitiesID": GPT3response(
+                prompt, client
+            ),  ## naming with IDs required for current evaluation script
         }
         results.append(result)
 
@@ -61,10 +69,22 @@ def run(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run the Model with Question and Fill-Mask Prompts")
-    parser.add_argument("-i", "--input", type=str, required=True, help="Input (subjects) file")
-    parser.add_argument("-o", "--output", type=str, required=True, help="Predictions (output) file")
-    parser.add_argument("-k", "--oaikey", type=str, required=True, help="OpenAI API key")
+    parser = argparse.ArgumentParser(
+        description="Run the Model with Question and Fill-Mask Prompts"
+    )
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="Input (subjects) file"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help="Predictions (output) file",
+    )
+    parser.add_argument(
+        "-k", "--oaikey", type=str, required=True, help="OpenAI API key"
+    )
 
     args = parser.parse_args()
 
